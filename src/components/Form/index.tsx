8a34646f82be8311f11ffa0af5921525a0b1de01
¨Matheus¨ <ymatheus.h@gmail.com>
2023-05-27 02:56:51 -0300

    feat: :sparkles: implements props todoText, setTodoText, handleCreateNewTodo to Form component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,7 +3,13 @@ import Hug from '../../assets/hug.svg';
 
 import { styles } from './styles';
 
-export function Form() {
+type Props = {
+  toDoText: string;
+  setToDoText: (text: string) => void;
+  handleCreateNewTodo: () => void;
+}
+
+export function Form({ toDoText, setToDoText, handleCreateNewTodo }: Props) {
   return (
     <View
       style={styles.form}
@@ -12,10 +18,13 @@ export function Form() {
         style={styles.input}
         placeholder='Adicione uma nova tarefa'
         placeholderTextColor='#808080'
+        onChangeText={setToDoText}
+        value={toDoText}
       />
 
       <TouchableOpacity
         style={styles.button}
+        onPress={handleCreateNewTodo}
       >
         <Hug
           width={52}
