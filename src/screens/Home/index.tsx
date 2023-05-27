0105f574c261912847b38bfd01404541c80bec72
¨Matheus¨ <ymatheus.h@gmail.com>
2023-05-27 00:55:42 -0300

    feat: :sparkles: create screen Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header";
+
+import { Form } from "../../components/Form";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  )
+}
